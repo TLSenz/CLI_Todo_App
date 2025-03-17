2835c7b24afb8d8b1f8cb49973d2bad720b4f8d2
TLSenz <sven@zemp.email>
2025-03-17 11:14:56 +0100

    Added Feature Import from File, have to add that you can give the File path yourself

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
-use std::io::stdin;
+use std::io::{stdin, BufReader};
 use std::num::ParseIntError;
+use std::fs::File;
+use std::io::{self,BufRead};
+use std::io::ErrorKind::FileTooLarge;
 
 fn main() {
     let mut todo_list: Vec<Todo> = Vec::new();
@@ -11,18 +14,23 @@ fn main() {
             1 => create_todo(&mut todo_list),
             2 => see_todos(&todo_list),
             3 => mark_todo_as_done(&mut todo_list),
-            4 => break,  // Exit the loop
+            4 => {
+                let buffer_list = import_todos();
+                for n in buffer_list.into_iter() {
+                    todo_list.push(n);
+                }
+
+            }
+            5 => break,  // Exit the loop
             _ => println!("Invalid option, please try again."),
         }
     }
 }
-
 struct Todo{
     name : String,
     todo : String,
 
 }
-
 fn menu() -> i32{
     println!("Was moechtest du machen");
 
@@ -37,8 +45,6 @@ fn menu() -> i32{
     stdin().read_line(&mut user_input).expect("Coud not Read Commandline");
 
     let zahl: Result<i32, ParseIntError> = user_input.trim().parse::<i32>();
-    
-    
     match zahl {
         Ok(num) => {
             println!("Parsed {} sucessfull",num );
@@ -50,7 +56,6 @@ fn menu() -> i32{
         }
     }
 }
-
 fn see_todos(todo_list: &Vec<Todo>){
     for n in todo_list.iter(){
         println!("{}",n.name);
@@ -58,8 +63,6 @@ fn see_todos(todo_list: &Vec<Todo>){
 
     }
 }
-
-
 fn mark_todo_as_done( todo_list: & mut Vec<Todo>){
     for (index , todo) in todo_list.iter().enumerate(){
         println!("{}, {}", index, todo.todo)
@@ -100,4 +103,48 @@ fn create_todo( todo_list:&mut Vec<Todo>){
     todo_list.push(todo);
 
 
+}
+fn import_todos() -> Vec<Todo>{
+    let result = read_todos_from_file();
+
+    let todo_in_string_format: Vec<String> =  match result {
+        Ok(todo_in_string_format) => todo_in_string_format,
+        Err(error) => {
+            println!("Error while reading file: {}",error);
+            Vec::new()
+        }
+    };
+    let todos:Vec<Todo> = todo_in_string_format
+        .into_iter()
+        .map(|line| {
+            let parts: Vec<&str> = line.split(",").collect()
+            if parts.len() == 2{
+                Todo{
+                    name : parts[0].to_string(),
+                    todo : parts[1].to_string()
+                }
+            }
+            else {
+                Todo{
+                    name: String::new(),
+                    todo : String::new()
+
+                }
+            }
+        })
+    .collect();
+
+    todos
+}
+fn read_todos_from_file() -> Vec<String>{
+    let file = File::open(r"C:\Users\zempsv\Documents\RustTodo.txt");
+    let reader =   io::BufReader::new(file);
+    let mut file_input:Vec<String> = Vec::new();
+
+
+    for line in reader.lines(){
+        let line = line?;
+        file_input.push(line);
+    }
+    file_input
 }
